7937815b3709ac22ebab32369eacd87711e00982
João Victor <joaoparronchi04@gmail.com>
2023-04-03 20:31:11 -0300

    adicionando um pouco de responsividade e a troca do botão de navegação

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-function ativaLetra(elemento){
-    const arrayList = elemento.innerHTML.split('')/*criando uma lista(arrayList) com o conteudo do h1(elemento) e usando uma ferramenta que faz a separação das letras(split)*/
+function digitando(){
 
-    /*console.log(arrayList) para ver como fica*/
+    function ativaLetra(elemento){
+        const arrayList = elemento.innerHTML.split('')/*criando uma lista(arrayList) com o conteudo do h1(elemento) e usando uma ferramenta que faz a separação das letras(split)*/
+    
+        /*console.log(arrayList) para ver como fica*/
+    
+        elemento.innerHTML = '';/*limpando o array para que apareça apenas com a função forEach*/
+    
+        arrayList.forEach((letra, index)=> {/*criando um looping*/
+            setTimeout(()=> {
+                elemento.innerHTML += letra
+            }, 75 * index)/*função que traz uma letra a cada 75ms usando o setTimeout*/
+        });
+    }
+    
+    const titulo = document.querySelector('.digitando')/*armazenando o h1 na variavel titulo*/
+    
+    ativaLetra(titulo)/*chamando a função e o elemento com o conteudo (o titulo)*/
 
-    elemento.innerHTML = '';/*limpando o array para que apareça apenas com a função forEach*/
-
-    arrayList.forEach((letra, index)=> {/*criando um looping*/
-        setTimeout(()=> {
-            elemento.innerHTML += letra
-        }, 75 * index)/*função que traz uma letra a cada 75ms usando o setTimeout*/
-    });
 }
+digitando()
 
-const titulo = document.querySelector('.digitando')/*armazenando o h1 na variavel titulo*/
+function trocaIcon(){
 
-ativaLetra(titulo)/*chamando a função e o elemento com o conteudo (o titulo)*/
+    const ativaMenu = document.querySelector('.fa-bars')/*armazenando tag i*/
+    ativaMenu.addEventListener('click', ()=>{/*criando um evento de click e uma função para a troca da tag i*/
+        ativaMenu.classList.toggle('fa-x')
+    })
+
+}
+trocaIcon()
 
 /*Leia tudo para tentar entender melhor, João*/
